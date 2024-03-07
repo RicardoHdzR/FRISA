2530f6d21a4e38b4b56df75238112256c303f360
martnreh <martinbarrientos451@gmail.com>
2024-03-07 17:51:35 -0600

    ..

--- web-app/src/pages/index.jsx
+++ web-app/src/pages/index.jsx
@@ -1,29 +1,65 @@
-import React from 'react'
-import { useRouter } from 'next/router'
-import { Container, Card, ListGroup} from 'react-bootstrap'
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { Container, Card, Spinner, Row, Col } from 'react-bootstrap';
 
 function Home() {
-  const router = useRouter()
-  
+  const router = useRouter();
+  const [modules, setModules] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const getUser = async () => {
+    try {
+      const response = await fetch('/api/course');
+      const data = await response.json();
+      setModules(data.data);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching modules:', error);
+      setLoading(false);
+    }
+  };
 
   return (
     <Container>
-        <h1>Página de Materias</h1>
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              Selecciona la materia que te gustaría estudiar
-            </Card.Title>
 
-            <ListGroup>
-              <ListGroup.Item action onClick={() => router.push('/materia/1')}>De la Información al Conocimiento</ListGroup.Item>
-              <ListGroup.Item action onClick={() => router.push('/materia/2')}>El lenguaje en la Relación del Hombre con el Mundo</ListGroup.Item>
-              <ListGroup.Item action onClick={() => router.push('/materia/3')}>Textos y Visiones del Mundo</ListGroup.Item>
-            </ListGroup>
-          </Card.Body>
-        </Card>
+      <Row>   
+        <i class="bi bi-file-earmark"></i>
+        <h1 className="mt-4 mb-4">Bloque 1</h1>
+      </Row>
+      {loading ? (
+        <div>
+          <Spinner animation="border" role="status"></Spinner>
+          <p>Cargando...</p>
+        </div>
+      ) : (
+        <Row>
+        {modules.map((module) => (
+          <Col key={module.id} xs={12} sm={6} md={4} lg={3}>
+              <Card className="mb-3 hover-overlay shadow-1-strong" onClick={() => router.push(`/materia/${module.id}`)}>
+              <Card.Header className='bg-danger text-white' >
+                <strong>{module.name}</strong>
+              </Card.Header>
+              <Card.Body>
+                <Card.Title>{module.description}</Card.Title>
+              </Card.Body>
+              <Card.Footer className="text-muted"> 0/10</Card.Footer>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+        
+      )}
+      <style jsx>{`
+        .card:hover {
+          background-color: lightgray;
+        }
+      `}</style>
     </Container>
-  )
+  );
 }
 
-export default Home
+export default Home;
