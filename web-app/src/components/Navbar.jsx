f475c5e381ad073aefc587225bb837e5289fef88
martnreh <martinbarrientos451@gmail.com>
2024-03-10 13:38:24 -0600

    navbar image

--- web-app/src/components/Navbar.jsx
+++ web-app/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Navbar, Nav, Image } from 'react-bootstrap'
 function _Navbar() {
   return (
     <Navbar className="bg-body-tertiary" fixed='top' >
-        <Navbar.Brand href='/'><Image src='/logo.png' className='w-50 m-0'></Image></Navbar.Brand>
+        <Navbar.Brand href='/' ><Image src='/logo.png' className='w-75 m-0'></Image></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         
     </Navbar>
