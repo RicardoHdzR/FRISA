4df6dd989829fc48f429cfed59a2056b6b02592e
martnreh <martinbarrientos451@gmail.com>
2024-03-09 22:12:51 -0600

    ..

--- web-app/src/pages/logIn.jsx
+++ web-app/src/pages/logIn.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Card, Form, Row, Button, Container } from 'react-bootstrap';
 import { useRouter } from 'next/router';
+import _Navbar from '../components/Navbar';
+
 
 function LogIn() {
     const router = useRouter();
@@ -26,10 +28,9 @@ function LogIn() {
         const userData = await response.json();
 
         if (userData.data.length === 0) {
-            setErrorMessage('Email o contraseña inválido. Intentalo de nuevo.');
+            setErrorMessage('Email o contraseña inválido. Inténtalo de nuevo.');
         }
         setUser(userData.data[0]);
-        
     };
 
     useEffect(() => {
@@ -39,49 +40,67 @@ function LogIn() {
     }, [user]);
 
     return (
-        <Container>
-            <Card className="justify-content-center d-flex">
-                <Card.Body>
-                    <Form>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Correo Electrónico</Form.Label>
-                            <Form.Control
-                                type="email"
-                                placeholder="nombre@ejemplo.com"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Contraseña</Form.Label>
-                            <Form.Control
-                                type="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                        </Form.Group>
+        <Container fluid className="p-0 m-0" style={{ height: '100vh' }}>
+            <_Navbar />
 
-                        <Row className="mb-3">
-                            <Button variant="primary" onClick={handleLogIn}>
-                                Iniciar Sesión
-                            </Button>
-                        </Row>
-                        {errorMessage && (
+            <div
+                style={{
+                    backgroundImage: `linear-gradient(
+                        rgba(0, 0, 0, 0.70), 
+                      rgba(0, 0, 0, 0.70)
+                      ), url('https://info.insidetracker.com/hubfs/man%20computer%20wellness%20myths.jpg')`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    minHeight: '100vh',
+                    minWidth: '100vw',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <Card>
+                    <Card.Body className='m-4'>
+                        <Form>
+                            <Form.Group className="mb-3">
+                                <Form.Label><strong>Correo Electrónico</strong></Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    placeholder="nombre@ejemplo.com"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label><strong>Contraseña</strong></Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </Form.Group>
+
                             <Row className="mb-3">
-                                <p className="text-danger">{errorMessage}</p>
+                                <Button variant="primary" onClick={handleLogIn}>
+                                    Iniciar Sesión
+                                </Button>
                             </Row>
-                        )}
-                        <Row className="mb-3">
-                            <Card.Text>¿No tienes una Cuenta?</Card.Text>
-                        </Row>
-                        <Row className="mb-3">
-                            <Button variant="info" onClick={() => router.push('/signIn')}>
-                                Registrarse
-                            </Button>
-                        </Row>
-                    </Form>
-                </Card.Body>
-            </Card>
+                            {errorMessage && (
+                                <Row className="mb-3">
+                                    <p className="text-danger">{errorMessage}</p>
+                                </Row>
+                            )}
+                            <Row className="mb-3">
+                                <Card.Text>¿No tienes una Cuenta?</Card.Text>
+                            </Row>
+                            <Row className="mb-3">
+                                <Button variant="info" onClick={() => router.push('/signIn')}>
+                                    Registrarse
+                                </Button>
+                            </Row>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </div>
         </Container>
     );
 }
